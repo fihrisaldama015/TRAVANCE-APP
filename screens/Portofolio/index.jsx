b8a407b979fc118e26d2ca921aa2d430f3b2e8fd
mathzino <fairusramadhani29@gmail.com>
2022-11-24 17:25:48 +0700

    create Tab Navigation

---
+++ screens/Portofolio/index.jsx
@@ -0,0 +1,9 @@
+import { View, Text } from "react-native";
+
+export default function PortofolioScreen() {
+  return (
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Text>Portofolio</Text>
+    </View>
+  );
+}
